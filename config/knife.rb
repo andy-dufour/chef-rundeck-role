9ce9daabf64860cf74a15cfc11c3207a29d04d53
andy-dufour <andy.k.dufour@gmail.com>
2014-08-05 15:38:50 -0400

    Changing some values

--- config/knife.rb
+++ config/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
-node_name                'webtech'
-client_key               '/opt/chef-rundeck-role/config/webtech.pem'
+node_name                'client'
+client_key               'client.pem'
 validation_client_name   'chef-validator'
-validation_key           '/opt/chef-rundeck-role/config/chef-validator.pem'
-chef_server_url          'https://wadp001cnc.rim.net:443'
+validation_key           'chef-validator.pem'
+chef_server_url          ''
 syntax_check_cache_path  ''
 cookbook_path [ '' ]
